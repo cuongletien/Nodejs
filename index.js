c71e4e875f1e4d2b168ef17c4e76ba66c779c133
Le Cuong <cuonglt1408@gmail.com>
2020-04-06 20:55:37 +0700

    add pug and test demo

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+var users = [
+    {id:1, name:'Demo'},
+    {id:2, name:'Hello'}
+]
+
+
+app.get('/', (req, res) => res.render('index', {title: 'Hey', message: 'Hello Cuong!' }));
+app.get('/users', (req, res) => res.render('users/index', { users } ));
+
+app.get('/users/search', (req,res) => {
+    let valueQuery = req.query.q;
+    let matchedUsers = users.filter(item => item.name.indexOf(valueQuery) !== -1);
+    res.render('users/index', {
+        users : matchedUsers
+    });
+} );
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
